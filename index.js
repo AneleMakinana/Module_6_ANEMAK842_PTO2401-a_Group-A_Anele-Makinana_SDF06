83c61e2ad2aeefb1ab0dd18e7103b7b150fb00b4
AneleMakinana <makinanaa20@gmail.com>
2024-04-01 19:02:16 +0200

    Styled my page and added a photo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+const appSettings = {
+    databaseURL: "https://playground-361b9-default-rtdb.firebaseio.com/"
+}
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -9,5 +8,5 @@ const addButtonEl = document.getElementById("add-button")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    alert(inputValue);
 })
